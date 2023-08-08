2b239dc613abc3986a646b15b137b0059daabbc1
Naama Tamir <106441652+emma177@users.noreply.github.com>
2023-08-08 11:16:09 +0300

    manually run populateDB

--- populateDB.js
+++ populateDB.js
@@ -1,3 +1,6 @@
+// This script is used to populate the database from external sources.
+// It should be run manually and NOT automatically during deployment.
+
 const connectDB = require('./configs/db');
 const { populateMembersCollection } = require('./BLL/membersBLL');
 const { populateMoviesCollection } = require('./BLL/moviesBLL');
